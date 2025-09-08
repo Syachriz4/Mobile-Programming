0658f5add888a71cb57267dcc8a122f17fbf6dec
Bintang Syachriza Akbar <syachriz4@gmail.com>
2025-09-08 19:11:58 +0700

    Add folder modul_4

---
+++ modul_4/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        bottomNavigationBar:Container(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+          color: Colors.black54,
+          height: 125,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(child: Icon(Icons.shuffle, color: Colors.white, size: 30,)),
+                Expanded(child: Icon(Icons.skip_previous, color: Colors.white, size: 30,)),
+                Flexible(fit: FlexFit.tight,flex: 2, child: Icon(Icons.play_circle_fill, color: Colors.white, size: 60,)),
+                Expanded(child: Icon(Icons.skip_next, color: Colors.white, size: 30,)),
+                Expanded(child: Icon(Icons.repeat, color: Colors.white, size: 30,)),
+              ],
+            ),
+          ),
+        ),
+        body: Center(
+          child: Text('Pemutar Music'),
+        ),
+      ),
+    );
+  }
+}
